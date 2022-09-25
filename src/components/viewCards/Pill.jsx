53da04f54cc8be29efed811c5a0f5520d2e5f3f6
abhinav2188 <abhinav.mca19.du@gmail.com>
2022-09-25 18:17:50 +0530

    updated Pill style

--- src/components/viewCards/Pill.jsx
+++ src/components/viewCards/Pill.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 const Pill = ({children, style}) => (
-    <Text className="rounded-full py-1 px-2 bg-gray-600 font-light text-white" style={style}>
+  <View className="rounded-full py-1 px-2 bg-gray-600" style={style}>
+    <Text className="font-light text-white">
       {children}
     </Text>
+  </View>
 );
 
 
